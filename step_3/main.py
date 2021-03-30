648ccf11cd913404e616420dff8712a3b99a036c
Dominik Fretz <dominik.fretz@gmail.com>
2021-03-31 00:16:36 +1100

    Add module example

---
+++ step_3/main.py
@@ -0,0 +1,8 @@
+"""
+main.py
+"""
+import sensor
+
+sensor_data = sensor.read_sensor()
+
+print(sensor_data)
---
+++ step_3/sensor.py
@@ -0,0 +1,9 @@
+"""
+sensor.py
+Contains functions to interace a sensor
+"""
+
+
+def read_sensor():
+    # do something to read the sensor
+    return {"source": "sensor-01", "value": 4.2}
