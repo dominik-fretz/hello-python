2ffe9f9c684b5ef0145e48ed4fa22ed516620982
Dominik Fretz <info@codewithpassion.com>
2021-04-07 19:06:14 +1000

    Add ISBN

--- step_4-classes_2/book.py
+++ step_4-classes_2/book.py
@@ -3,6 +3,7 @@ class Book:
         self.title = title
         self.revision = revision
         self.authors = authors
+        self.isbn = "123-333"
 
     def lend(self, userName):
         print("Book {} lent by {}".format(self.title, userName))
